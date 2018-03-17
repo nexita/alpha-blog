28f044749b9667d5f3f0d43ed0d04be44ee2b5c6
abhishek <dhakalabhishek@gmail.com>
2018-03-17 14:58:13 +0545

    Create rails applicationa and setup home and about pages

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,11 @@
+class PageController < ApplicationController
+
+    def home
+        #creating a home page function
+    end
+
+    def about
+        #creating a about page function
+    end
+    
+end
